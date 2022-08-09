e0b6b469a74d19e6685b225cb1ba3881aca45bdd
ThesisSlayers <72562021+ThesisSlayers@users.noreply.github.com>
2022-08-09 16:15:15 -0400

    I added some more speed tests

--- src/quench_tests.jl
+++ src/quench_tests.jl
@@ -10,9 +10,10 @@ function get_P_Minkowski()
     In this test I calculate the transition probability as a function of Ω, of an inertial observer
     in flat spacetime with a gaussian switching function.
     """
-    d = 4*σ    
+    λ = 1.0
+    d = 5*σ    
     df = deform_funcs["cos2"]
-    ε_contour = 1e-1
+    ε_contour = 3e-1
 
     initial_τs, final_τs =  [-d, -d], [d, d]
     integrate(f::Function) = hcubature(f, initial_τs, final_τs, maxevals=100000 , rtol=int_tol)[1]
@@ -21,13 +22,13 @@ function get_P_Minkowski()
     W = DistributionWithTrajectories(_Ws["flat"], X, X)
     χ(τ) = χs["gauss"](τ/σ)
 
-    Ωs = LinRange(0, 3, 10)
+    Ωs = LinRange(0, 2.5, 30)
     P_Ms = []
     for (i, Ω) in enumerate(Ωs)
         println("\rDoing Ω number $i")
         l = get_l(W, λ, Ω, χ)
         # l = complexify_l_or_m(l, ε_contour)
-        l = complexify_l_or_m(l, df, distance_funcs["lorentz"], ε_contour)
+        l = complexify_l_or_m(l, df, distance_funcs["flat"], ε_contour)
         P_M = integrate(l)
         push!(P_Ms, P_M)
     end
@@ -35,7 +36,10 @@ function get_P_Minkowski()
     println("$x")
 
     P(Ω) = λ^2/4π*(exp(-σ^2*Ω^2) - √π*σ*Ω*erfc(σ*Ω))
-    display(plot(Ωs, [P.(Ωs), real.(P_Ms)]))
+
+    p = plot(Ωs, [P.(Ωs), real.(P_Ms), imag.(P_Ms)], labels=["Theoretical" "Real part of numerical" "Imaginary part of numerical"])
+    display(p)
+    savefig(p, raw"plots\P_vs_Ω_Minkowski.png")
 end
 
 function get_M_vs_Ω_Minkowski()
@@ -62,13 +66,16 @@ function get_M_vs_Ω_Minkowski()
         println("\rDoing Ω number $i: Ω = $Ω")
         m = get_m(D, λ, Ω, χ)
         # m = complexify_l_or_m(m, ε_contour)
-        m = complexify_l_or_m(m, df, distance_funcs["lorentz"], ε_contour)
+        m = complexify_l_or_m(m, df, distance_funcs["flat"], ε_contour)
         M = integrate(m)
         push!(Ms, M)
     end
 
     M(Ω) = im*(λ^2)*σ/(4*√π*L)*exp(-(σ*Ω)^2 - L^2/(4*σ^2))*(erf(im*L/(2σ)) - 1)
-    display(plot(Ωs, [(abs ∘ M).(Ωs), abs.(Ms)], labels=["theoretical" "numerical"]))
+
+    p = plot(Ωs, [(abs ∘ M).(Ωs), abs.(Ms)], labels=["theoretical" "numerical"])
+    display(p)
+    savefig(p, raw"plots\M_vs_Ω_Minkowski.png")
     return Ωs, Ms, M.(Ωs)
 end
 
@@ -79,12 +86,12 @@ function get_M_vs_L_Minkowski()
     """
     d = 5*σ    
     df = deform_funcs["cos2"]
-    ε_contour = 5e-3
+    ε_contour = 1e-1
 
     initial_τs, final_τs =  [-d, -d], [d, d]
     integrate(f::Function) = hcubature(f, initial_τs, final_τs, maxevals=100000 , rtol=int_tol)[1]
 
-    Ls = LinRange(0.001, 7, 30)
+    Ls = LinRange(0.001, 7, 20)
     Ms = []
     for (i, L) in enumerate(Ls)
         XA, XB = InertialTrajectory(0.0, 0.0, 0.0), InertialTrajectory(L, 0.0, 0.0)
@@ -94,13 +101,15 @@ function get_M_vs_L_Minkowski()
         println("\rDoing L number $i: L = $L")
         m = get_m(D, λ, Ω, χ)
         m = complexify_l_or_m(m, ε_contour)
-        # m = complexify_l_or_m(m, df, distance_funcs["lorentz"], ε_contour)
+        # m = complexify_l_or_m(m, df, distance_funcs["flat"], ε_contour)
         M = integrate(m)
         push!(Ms, M)
     end
 
     M(L) = im*(λ^2)*σ/(4*√π*L)*exp(-(σ*Ω)^2 - L^2/(4*σ^2))*(erf(im*L/(2σ)) - 1)
-    display(plot(Ls, [(abs ∘ M).(Ls), abs.(Ms)], labels=["theoretical" "numerical"], ylims=[-1e-6, 2e-4]))
+    p = plot(Ls, [(abs ∘ M).(Ls), abs.(Ms)], labels=["theoretical" "numerical"], ylims=[-1e-6, 2e-4])
+    display(p)
+    savefig(p, "plots\\M_vs_L_Minkowski\\ε_contour_$ε_contour.png")
     return Ls, Ms, M.(Ls)
 end
 
@@ -116,7 +125,7 @@ function plot_inertial_l()
     df = deform_funcs["triangle"]
     Ω = 1
     l = get_l(W, λ, Ω, χ)
-    l = complexify_l_or_m(l, df, distance_funcs["lorentz"], 1e-3)
+    l = complexify_l_or_m(l, df, distance_funcs["flat"], 1e-3)
     # l = complexify_l_or_m(l, 1e-5)
     
     _l(t) = l([t,0])
@@ -127,29 +136,29 @@ function plot_inertial_l()
 end
 
 function plot_inertial_m()
-#     """
-#     In this test I calculate the transition probability as a function of Ω, of an inertial observer
-#     in flat spacetime with a gaussian switching function.
-#     """
-#     L = 1.0
-#     XA, XB = InertialTrajectory(0.0, 0.0, 0.0), InertialTrajectory(L, 0.0, 0.0)
-#     W = DistributionWithTrajectories(_Ws["flat"], XA, XB)
-#     χ(τ) = χs["gauss"](τ/σ)
+    """
+    In this test I calculate the transition probability as a function of Ω, of an inertial observer
+    in flat spacetime with a gaussian switching function.
+    """
+    L = 1.0
+    XA, XB = InertialTrajectory(0.0, 0.0, 0.0), InertialTrajectory(L, 0.0, 0.0)
+    W = DistributionWithTrajectories(_Ws["flat"], XA, XB)
+    χ(τ) = χs["gauss"](τ/σ)
 
-#     df = deform_funcs["triangle"]
-#     Ω = 1
-#     m = get_m(W, λ, Ω, χ)
-#     m = complexify_l_or_m(m, df, distance_funcs["lorentz"], 1e-1)
-#     # m = complexify_l_or_m(l, 1e-5)
+    df = deform_funcs["triangle"]
+    Ω = 1
+    m = get_m(W, λ, Ω, χ)
+    m = complexify_l_or_m(m, df, distance_funcs["flat"], 1e-1)
+    # m = complexify_l_or_m(l, 1e-5)
     
-#     _m(t) = m([t,0])
-#     _m_th(t) = -λ^2*χ(t)*χ(0) * _Ds["flat"]([t, 0.0, 0.0, 0.0], [0.0, L, 0.0, 0.0])*exp(im*Ω*t)
-#     ts = LinRange(-5, 5, 60)
-#     display(plot(ts, [abs ∘ _m, abs ∘ _m_th]))#, ylims=[-4,4]))
+    _m(t) = m([t,0])
+    _m_th(t) = -λ^2*χ(t)*χ(0) * _Ds["flat"]([t, 0.0, 0.0, 0.0], [0.0, L, 0.0, 0.0])*exp(im*Ω*t)
+    ts = LinRange(-5, 5, 200)
+    display(plot(ts, [abs ∘ _m, abs ∘ _m_th]))#, ylims=[-4,4]))
 end
 
-get_P_Minkowski();
+# get_P_Minkowski();
 # Ωs, Ms_num, Ms_th = get_M_vs_Ω_Minkowski();
-# Ls, Ms_num, Ms_th = get_M_vs_L_Minkowski();
+Ls, Ms_num, Ms_th = get_M_vs_L_Minkowski();
 # plot_inertial_l();
 # plot_inertial_m();
