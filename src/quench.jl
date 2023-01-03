b362d3c0a18276e98044961a4ae6fae7b6412a08
Adrian Lopez <adrian.lopez.r753@gmail.com>
2023-01-03 16:03:00 -0500

    Working code. Added separate sqitching functions for both trajectories. Moved complexify inised get_m/ls. Now W and D get contour deformed, not m and ls. Added rindler.

--- src/quench.jl
+++ src/quench.jl
@@ -1,43 +1,44 @@
-using HCubature, Plots, ProgressBars#, QuantumInformation
+using HCubature, Plots, ProgressBars, DataFrames#, QuantumInformation
 include("params.jl")
 # include("tools/misc.jl")
 # include("tools/1_Trajectories.jl")
 # include("tools/2_D&W.jl")
 include("tools/3_LM_getters.jl")
 include("tools/SwitchingFuncs.jl")
-include("tools/DeformFuncs.jl")
 
-χ(τ) = switching_funcs[switching_func_name]((τ-switching_function_center)/σ)
-df = deform_funcs[deform_func_name]
-dist_func = distance_funcs[space_time]
+function get_trajs(space_time, χ0A, χ0B, b)
+    if     space_time=="quench"  XA, XB = QuenchTrajectory(χ0A, b)   , QuenchTrajectory(χ0B, b)
+    elseif space_time=="flat"    XA, XB = InertialTrajectory(0, 0, 0), InertialTrajectory(χ0B, 0, 0) 
+    elseif space_time=="rindler" XA, XB = AcceleratedTrajectory(χ0A,0,0), AcceleratedTrajectory(χ0B,0,0) end
+    return XA, XB
+end
 
+χs = initialize_switching_funcs(switching_func_name, σ, switching_function_center_A, switching_function_center_B)  
 integrate(f) = hcubature(f, initial_τs, final_τs, maxevals=maxevals, rtol=int_tol)[1]
-Cplify(l_or_m) = complexify_l_or_m(l_or_m, ε_contour) # I'm writing this to shorten the name and to be able to use map_dict that requires a functions with a single argument
 
 ρs = []
 Cs, Ns, = zeros(length(Ωs), length(χ0Bs)), zeros(length(Ωs), length(χ0Bs))
-# PAs, PBs = zeros(length(Ωs)), zeros(length(Ωs), length(χ0Bs))
+PAs, PBs = zeros(length(Ωs)), zeros(length(Ωs), length(χ0Bs))
 for (i, Ω) in tqdm(enumerate(Ωs))
     for (j, χ0B) in tqdm(enumerate(χ0Bs))
-        if     space_time=="quench" XA, XB = QuenchTrajectory(χ0A, b)   , QuenchTrajectory(χ0B, b)
-        elseif space_time=="flat"   XA, XB = InertialTrajectory(0, 0, 0), InertialTrajectory(χ0B, 0, 0) end
-        Ws, D = initialize_distributions(_Ws[space_time], _Ds[space_time], XA, XB)
+        XA, XB = get_trajs(space_time, χ0A, χ0B, b)
+        Ws, D  = initialize_distributions(_Ws[space_time], _Ds[space_time], XA, XB)
         if with_derivative_coupling Ws, D = add_crossed_derivatives(Ws, D, ε_numeric_derivative) end
 
-        m, ls = get_m(D, λ, Ω, χ), get_ls(Ws, λ, Ω, χ)
-        m, ls = Cplify(m)        , map_dict(Cplify, ls)
-        M, Ls = integrate(m)     , map_dict(integrate, ls)
+        m, ls = get_m(D, λ, Ω, χs, ε_contour), get_ls(Ws, λ, Ω, χs, ε_contour)
+        M, Ls = integrate(m)                 , map_dict(integrate, ls)
 
-        ρ = [1 - Ls["AA"] - Ls["BB"]              0          0   conj(M);
-                                   0       Ls["AA"]   Ls["AB"]         0;
-                                   0  conj(Ls["AB"])  Ls["BB"]         0;
-                                   M              0          0         0]
+#                                 00        01              10        11
+        ρ = [1 - Ls["AA"] - Ls["BB"]         0               0   conj(M); #00
+                                   0  Ls["BB"]   conj(Ls["AB"])        0; #01
+                                   0  Ls["AB"]        Ls["AA"]         0; #10
+                                   M         0               0         0] #11
 
         Cs[i,j] = concurrence(ρ)
         Ns[i,j] = negativity(ρ)
         push!(ρs, ρ)
-        # PAs[i] = real(ρ[2,2])
-        # PBs[i,j] = real(ρ[3,3])
+        PAs[i]   = real(ρ[2,2])
+        PBs[i,j] = real(ρ[3,3])
     end
 end
 
