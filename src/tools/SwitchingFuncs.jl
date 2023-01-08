f02bf4f999b3fbcd437ffc9e64b155c90ea4ab3a
Adrian Lopez <adrian.lopez.r753@gmail.com>
2023-01-08 13:51:57 -0500

    Corrected the cos4 function which had a width of 2 instead of 1

--- src/tools/SwitchingFuncs.jl
+++ src/tools/SwitchingFuncs.jl
@@ -1,8 +1,12 @@
-_cos4( τ)::Complex = abs(real(τ)) <= 1 ? cos(π*τ/2)^4 : 0.0
+_cos4( τ)::Complex = abs(real(τ)) <= 1/2 ? cos(π*τ)^4 : 0.0
 _gauss(τ)::Complex = exp(-τ^2/2) 
 
 switching_funcs = Dict("cos4"  => _cos4,
                        "gauss" => _gauss,
                        "one"   => x -> 1.0)
 
-
+function initialize_switching_funcs(switching_func_name, σ, switching_function_center_A, switching_function_center_B)  
+    χA(τ) = switching_funcs[switching_func_name]((τ - switching_function_center_A)/σ)
+    χB(τ) = switching_funcs[switching_func_name]((τ - switching_function_center_B)/σ)
+    [χA, χB]
+end
