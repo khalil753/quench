7966142c0960b954ebbf8c670737e0f65e2d7456
Adrian Lopez <adrian.lopez.r753@gmail.com>
2023-01-28 21:09:44 -0500

    Corrected the quench trajectory so it depends on the proper time

--- src/tools/1_Trajectories.jl
+++ src/tools/1_Trajectories.jl
@@ -6,9 +6,10 @@ abstract type AbstractTrajectory <: Function end
   χ0::Fl
   y0::Fl
   b ::Fl
-  QuenchTrajectory(χ0::Fl, b::Fl) = new(χ0, asinh(χ0/b), b)
+  sqrt_chi_b::Fl
+  QuenchTrajectory(χ0::Fl, b::Fl) = new(χ0, asinh(χ0/b), b, sqrt(χ0^2 + b^2))
 end
-(X::QuenchTrajectory)(τ) = real(τ) > 0 ? [τ/X.χ0, X.y0] : [τ, X.χ0]
+(X::QuenchTrajectory)(τ) = real(τ) > 0 ? [τ/X.sqrt_chi_b, X.y0] : [τ, X.χ0]
 
 @def_structequal struct InertialTrajectory <: AbstractTrajectory
     x0::Fl
@@ -23,13 +24,12 @@ end
   y0::Fl
   z0::Fl
 end
-(X::AcceleratedTrajectory)(τ) = [X.χ0*sinh(τ/X.χ0), X.χ0*cosh(τ/X.χ0) + X.x0 - X.χ0, X.y0, X.z0]
+(X::AcceleratedTrajectory)(τ) = [X.χ0*sinh(τ/abs(X.χ0)), X.χ0*cosh(τ/abs(X.χ0)) + X.x0 - X.χ0, X.y0, X.z0]
 
 get_γ(X::AbstractTrajectory)    = (println("You haven't created the gamma function for this trajectory"), throw(Exception))
-# get_γ(X::AbstractTrajectory)    = τ -> ((X(τ + 1e-5) - X(τ - 1e-5))/2e-5)[1]; println("You're using a gamma that's calculated numerically")
 get_γ(X::InertialTrajectory)    = τ -> 1
-# get_γ(X::QuenchTrajectory)      = τ -> 1/X.χ0
-get_γ(X::QuenchTrajectory)      = τ -> cosh(τ/X.χ0)
+get_γ(X::QuenchTrajectory)      = τ -> 1/X.χ0
+# get_γ(X::QuenchTrajectory)      = τ -> cosh(τ/X.χ0)
 # get_γ(X::AcceleratedTrajectory) = τ -> cosh(τ/X.χ0)
 # get_γ(X::AcceleratedTrajectory) = τ -> 1
 
