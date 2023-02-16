635e585871c891b47a4b03fb1cf5de7ccfd18062
Adrian Lopez <adrian.lopez.r753@gmail.com>
2023-02-16 15:32:32 -0500

    Added the experiment_name param

--- src/params.jl
+++ src/params.jl
@@ -1,29 +1,32 @@
 using DataFrames
 
+const experiment_name = "large_chi_limit"
+
 # WightmanFunction params
-const space_time = "quench"
+const space_time = "flat"
 
 if     space_time == "quench"  const with_derivative_coupling = true
 elseif space_time == "rindler" const with_derivative_coupling = true 
-elseif space_time == "flat"    const with_derivative_coupling = false end
+elseif space_time == "flat"    const with_derivative_coupling = true end
 
 # Switching function params
 const switching_func_name = "cos4"
 const σ = 1.0
-const switching_function_center_A = 0.5σ
-const switching_function_center_B = 0.5σ
+const switching_function_center_A = 0.6σ
+const switching_function_center_B = 0.6σ
 
 # Quench regulator
-const b = 0.01
+const b = 1e-1
 
 # Detector frequencies and Initial positions
-nΩ = 4 # Number of frquencies/initial conditions to iterate over
-nχ = 8
-const χ0A = 1σ 
+nΩ = 15 # Number of frquencies/initial conditions to iterate over
+nχ = 15
+const χ0A = 20σ 
 if χ0A == 0 println("χ0A is zero and that will create problems with rindler and quench"); throw(Exception) end
 if     space_time in ["quench", "rindler"]  Ω0, Ωf, χ0B0, χ0Bf = -1/σ, 30/σ, χ0A + 0.5σ, χ0A + 1.5σ
 elseif space_time == "flat"                 Ω0, Ωf, χ0B0, χ0Bf =  5/σ, 20/σ,       0.5σ,         2σ end
 Ωs, χ0Bs = LinRange(Ω0, Ωf, nΩ), LinRange(χ0B0, χ0Bf, nχ)
+
 # Coupling strength of detector
 const λ = 1e-2
 
@@ -40,7 +43,7 @@ else                     const initial_τs, final_τs = [switching_function_cent
                                                       [switching_function_center_A + Δτ, switching_function_center_B + Δτ]
 end
 const rtol = 1e-4
-const maxevals = 500000
+const maxevals = 1000000
 
 # Complex contour params
 const ε_contour = 1e-2
@@ -52,8 +55,8 @@ params = DataFrame("Space_Time"           => [space_time],
                    "SF_Center_A"          => [switching_function_center_A],
                    "SF_Center_B"          => [switching_function_center_B],
                    "b"                    => [b],
-                   "nΩ"                   => [20],
-                   "nχ"                   => [20],
+                   "nΩ"                   => [nΩ],
+                   "nχ"                   => [nχ],
                    "χ0A"                  => [χ0A],
                    "Ω0"                   => [Ω0], 
                    "Ωf"                   => [Ωf], 
@@ -63,6 +66,6 @@ params = DataFrame("Space_Time"           => [space_time],
                    "Numeric_Derivative_ε" => [ε_numeric_derivative],
                    "initial_τs"           => [initial_τs],
                    "final_τs"             => [final_τs],
-                   "rtol"              => [rtol],
+                   "rtol"                 => [rtol],
                    "maxevals"             => [maxevals],
                    "Contour_ε"            => [ε_contour])
